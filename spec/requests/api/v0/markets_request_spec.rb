c2c10b982f8be9a7a086c16200c6c8a90288d761
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-15 16:36:55 -0600

    Test: write tests for vendor_count

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -20,16 +20,53 @@ RSpec.describe 'Markets API' do
 
         expect(market[:attributes]).to be_a(Hash)
         expect(market[:attributes]).to have_key(:name)
+        expect(market[:attributes][:name]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:street)
+        expect(market[:attributes][:street]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:city)
+        expect(market[:attributes][:city]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:county)
+        expect(market[:attributes][:county]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:state)
+        expect(market[:attributes][:state]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:zip)
+        expect(market[:attributes][:zip]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:lat)
+        expect(market[:attributes][:lat]).to be_a(String)
+
         expect(market[:attributes]).to have_key(:lon)
+        expect(market[:attributes][:lon]).to be_a(String)
         end
       end
 
-    it 'returns a new attribute called vendor_count'
+    it 'returns a new attribute called vendor_count' do
+      market_1 = create(:market)
+      market_2 = create(:market)
+      vendors = create_list(:vendor, 5)
+
+      create(:market_vendor, market_id: market_1.id, vendor_id: vendors[0].id)
+      create(:market_vendor, market_id: market_1.id, vendor_id: vendors[1].id)
+      create(:market_vendor, market_id: market_1.id, vendor_id: vendors[2].id)
+      
+      create(:market_vendor, market_id: market_2.id, vendor_id: vendors[3].id)
+      create(:market_vendor, market_id: market_2.id, vendor_id: vendors[4].id)
+      
+      get '/api/v0/markets'
+
+      markets = JSON.parse(response.body, symbolize_names: true)
+
+      expect(markets[:data][0][:attributes][:vendor_count]).to eq(3)
+      expect(market_1.vendors.count).to eq(3)
+      
+      expect(markets[:data][1][:attributes][:vendor_count]).to eq(2)
+      expect(market_2.vendors.count).to eq(2)
+      end
+    end
   end
 end
