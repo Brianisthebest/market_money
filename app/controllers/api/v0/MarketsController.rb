942e7220edb458f6ae6f6fc8ebdfcf2ce2dc5b39
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-16 10:27:17 -0600

    Feat: passing tests for show action

--- app/controllers/api/v0/MarketsController.rb
+++ app/controllers/api/v0/MarketsController.rb
@@ -4,14 +4,15 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   def show
-    # require 'pry'; binding.pry
     market = Market.find_by_id(params[:id])
     if market.nil?
-      render json: { code: 404,
-                     status: "Not Found",
-                     "errors": [{ 
-                     "detail": "Couldn't find Market with 'id'=#{params[:id]}" }]
-                    }
+      render json: {
+                     "errors": [
+                      { 
+                        "detail": "Couldn't find Market with 'id'=#{params[:id]}" 
+                      }]
+                    },
+                    status: 404
     else
       render json: MarketSerializer.new(Market.find(params[:id]))
     end
