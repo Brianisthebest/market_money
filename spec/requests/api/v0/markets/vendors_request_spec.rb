8aedf430fdd34f7c1cb47a05f2f4e67d01904657
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-16 11:16:33 -0600

    Test: add tests for all vendors associated to a market

--- spec/requests/api/v0/markets/vendors_request_spec.rb
+++ spec/requests/api/v0/markets/vendors_request_spec.rb
@@ -15,7 +15,42 @@ RSpec.describe 'Markets Vendors API' do
       create(:market_vendor, market_id: market_2.id, vendor_id: vendors[4].id)
 
       get "/api/v0/markets/#{market_1.id}/vendors"
+
+      expect(response).to be_successful
       
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      json[:data].each do |vendor|
+        expect(vendor).to have_key(:id)
+        expect(vendor[:id]).to be_an(String)
+
+        expect(vendor).to have_key(:type)
+        expect(vendor[:type]).to eq("vendor")
+
+        expect(vendor).to have_key(:attributes)
+        expect(vendor[:attributes]).to be_a(Hash)
+
+        expect(vendor[:attributes]).to have_key(:name)
+        expect(vendor[:attributes][:name]).to be_a(String)
+        
+        expect(vendor[:attributes]).to have_key(:description)
+        expect(vendor[:attributes][:description]).to be_a(String)
+        
+        expect(vendor[:attributes]).to have_key(:contact_name)
+        expect(vendor[:attributes][:contact_name]).to be_a(String)
+        
+        expect(vendor[:attributes]).to have_key(:contact_phone)
+        expect(vendor[:attributes][:contact_phone]).to be_a(String)
+        
+        expect(vendor[:attributes]).to have_key(:credit_accepted)
+        expect(vendor[:attributes][:credit_accepted]).to be_in([true, false])
+      end
+    end
+    
+    it 'sad path: returns 404 if market does not exist' do
+      get '/api/v0/markets/1/vendors'
+
+      expect(response.status).to eq(404)
     end
   end
 end
