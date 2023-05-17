f2038264548d8a052729279502eb02da99e58142
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-16 19:49:58 -0600

    Test: add tests for market vendor creation

--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.describe 'Market Vendors API' do
   describe 'POST /api/v0/market_vendors' do
     it 'can create a market vendor' do
-      market_1 = create(:market)
+      market_1 = create(:market, name: "Test Market")
       vendor_1 = create(:vendor)
       vendor_2 = create(:vendor)
       market_vendor_params = ({ "market_id": market_1.id, "vendor_id": vendor_1.id })
 
       headers = {"CONTENT_TYPE" => "application/json"}
 
-      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vender: market_vendor_params)
-    
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+      
+      created_market_vendor = MarketVendor.last
+
       expect(response).to be_successful
+      expect(response.status).to eq(201)
     end
   end
 end
