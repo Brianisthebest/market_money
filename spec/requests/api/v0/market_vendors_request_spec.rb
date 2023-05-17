a1d5831536078204ce059810fa1e88a1f26a062f
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-16 18:30:36 -0600

    Test: beginning test for market vendor create

--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Market Vendors API' do
-  
+  describe 'POST /api/v0/market_vendors' do
+    it 'can create a market vendor' do
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+      vendor_2 = create(:vendor)
+      market_vendor_params = ({ "market_id": market_1.id, "vendor_id": vendor_1.id })
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vender: market_vendor_params)
+    
+      expect(response).to be_successful
+    end
+  end
 end
