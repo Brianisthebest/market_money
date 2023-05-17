afa9f702b705a19ac958b0919b8fcf94961b6bef
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-17 11:36:42 -0600

    Feat: update create action in controller for sad path

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,10 +1,27 @@
 class Api::V0::MarketVendorsController < ApplicationController
   def create
     market_vendor = MarketVendor.new(market_vendor_params)
-require 'pry'; binding.pry
+
     if market_vendor.save
       render json: MarketVendorSerializer.new(market_vendor), status: 201
+    elsif market_vendor.errors.attribute_names.include?(:market) || market_vendor.errors.attribute_names.include?(:vendor)
+      render json: {
+        "errors": [
+          {
+            "detail": "Validation failed: #{market_vendor.errors.full_messages.to_sentence}"
+          }]
+      },
+      status: 404
+    else
+      render json: {
+        "errors": [
+          {
+            "detail": "Validation failed: Market vendor asociation between market with market_id=#{params[:market_vendor][:market_id]} and vendor_id=#{params[:market_vendor][:vendor_id]} already exists"
+          }]
+      },
+      status: 422
     end
+    require 'pry'; binding.pry
   end
 
   private
