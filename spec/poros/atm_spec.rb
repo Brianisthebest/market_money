4a7dc8e1b99cbe3b9a6989813c56ea7ab91377d5
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-18 14:51:43 -0600

    Fix: update poros test knowing how deelpy nested data is now

--- spec/poros/atm_spec.rb
+++ spec/poros/atm_spec.rb
@@ -4,21 +4,20 @@ RSpec.describe Atm do
   describe 'atm' do
     it 'exists and has attributes' do
       data = {
-              name: "ATM",
-              address: "123 AMT St",
-              lat: 39.750783,
-              lon: -104.996435,
-              distance: 0.1
+        poi: { name: 'ATM' },
+        address: { freeformAddress: '123 AMT St' },
+        position: { lat: 39.750783, lon: -104.996435 },
+        dist: 0.1
       }
 
       atm = Atm.new(data)
 
       expect(atm).to be_a(Atm)
-      expect(atm.name).to eq(data[:name])
-      expect(atm.address).to eq(data[:address])
-      expect(atm.lat).to eq(data[:lat])
-      expect(atm.lon).to eq(data[:lon])
-      expect(atm.distance).to eq(data[:distance])
+      expect(atm.name).to eq(data[:poi][:name])
+      expect(atm.address).to eq(data[:address][:freeformAddress])
+      expect(atm.lat).to eq(data[:position][:lat])
+      expect(atm.lon).to eq(data[:position][:lon])
+      expect(atm.distance).to eq(data[:dist])
     end
   end
 end
---
+++ spec/requests/api/v0/market_atms_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Market ATMs API' do
+  describe 'GET /api/v0/markets/:id/nearest_atms' do
+    it 'returns a list of nearby ATMs' do
+      
+    end
+  end
+end
