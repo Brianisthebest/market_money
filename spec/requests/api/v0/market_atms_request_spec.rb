388f5a24e3e08e870b61913ae4dfd8d13179d930
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-18 15:28:37 -0600

    Test: finish test

--- spec/requests/api/v0/market_atms_request_spec.rb
+++ spec/requests/api/v0/market_atms_request_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe 'Market ATMs API' do
 
       get "/api/v0/markets/#{market.id}/nearest_atms"
 
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
       
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data]).to be_an(Array)
+      expect(json[:data]).to have_key(:type)
+      expect(json[:data][:type]).to eq("atm")
+      expect(json[:data][:attributes]).to have_key(:name)
+      expect(json[:data][:attributes]).to have_key(:address)
+      expect(json[:data][:attributes]).to have_key(:lat)
+      expect(json[:data][:attributes]).to have_key(:lon)
+      expect(json[:data][:attributes]).to have_key(:distance)
     end
   end
 end
